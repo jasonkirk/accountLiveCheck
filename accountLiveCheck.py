5b5cc2da15fc4710063259fc00918d84c8f0cef5
Jason Kirkpatrick <jasonrkirk@gmail.com>
2016-03-14 17:32:42 -0500

    Got code working though needs to be cleaned up

--- accountLiveCheck.py
+++ accountLiveCheck.py
@@ -1,12 +1,12 @@
 import socket
 #mypath = '/var/cpanel/users/'
-mypath = 'd:/programming/accountLiveCheck/test/' # For test purposes
+mypath = './test/' # For test purposes
 from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 for account in onlyfiles:
-    print(account)
+    #print(account)
     with open(mypath + account) as f:
         content = f.readlines()
     for line in content:
@@ -19,14 +19,22 @@ for account in onlyfiles:
             line = line.split("=",1)[-1]
             line = line.rstrip('\r\n') #removes return line from string
             storedDNS = line
+            if ".res" in storedDNS:
+                break
             #print(storedDNS)
 
-            currentIP = socket.gethostbyname(storedDNS)
+    try:
+        oldIP = socket.gethostbyname(storedDNS)
+    except socket.gaierror:
+        oldIP = "0.0.0.0"
+    currentIP = storedIP
+    if oldIP:
 
-            if (storedDNS != currentIP):
-                print(storedDNS + " IP doesn't match")
-            else:
-                print(storedDNS + " IP matches")
+        if (oldIP != currentIP):
+            print(storedDNS + " " + currentIP + " " + oldIP + " IP doesn't match")
+
+    #        else:
+    #            print(storedDNS + " IP matches")
 
 
 
